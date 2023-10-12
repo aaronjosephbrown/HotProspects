ebdbfedea91d9540e07225aed9e9c7269d4c6a20
Aaron J. Brown <aaron.joseph.brown@me.com>
2023-10-11 20:23:45 -0500

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Aaron Brown on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Aaron Brown on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
