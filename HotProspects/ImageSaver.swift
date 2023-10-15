977bb7c50ace7b19c491b5b7b073de6c60b245cc
Aaron J. Brown <aaron.joseph.brown@me.com>
2023-10-15 13:56:50 -0500

    Added additional functionality.

---
+++ HotProspects/ImageSaver.swift
@@ -0,0 +1,8 @@
+//
+//  ImageSaver.swift
+//  HotProspects
+//
+//  Created by Aaron Brown on 10/14/23.
+//
+
+import Foundation
---
+++ HotProspects/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  HotProspects
+//
+//  Created by Aaron Brown on 10/15/23.
+//
+
+import Foundation
